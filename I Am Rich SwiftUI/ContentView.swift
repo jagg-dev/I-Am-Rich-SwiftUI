769e2d010b9e5946e4637a8cdf5e0474a4621519
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-05-24 23:47:39 -0500

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by Jorge Gonzalez on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
